6c000142d3ca0f43e843f375ca63dd930a72aa7a
krialex <56829037+krialex@users.noreply.github.com>
2024-08-29 12:18:52 +0200

    updated live server url in actions

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,9 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(/*on, config*/) {
       // implement node event listeners here
-    },
-    baseUrl: process.env.CYPRESS_BASE_URL || 'http://127.0.0.1:5500', 
+    }, 
     clearCookiesBeforeEach: true,
     clearLocalStorageBeforeEach: true,
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://127.0.0.1:5500',
   },
 });
